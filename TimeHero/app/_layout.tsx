5c3181b10a88573e9361f692b246eaeb553fb125
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2024-11-19 10:27:12 -0600

    Fix Redux Once and For All

--- TimeHero/app/_layout.tsx
+++ TimeHero/app/_layout.tsx
@@ -9,38 +9,14 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Provider } from 'react-redux';
-import { dispatch, store } from '../store/store';
-import { setUserId } from '@/store/userSlice';
+import { store } from '../store/store';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkLoginStatus = async () => {
-      const loggedIn = await AsyncStorage.getItem('isLoggedIn');
-      const userId = await AsyncStorage.getItem('userId');
-
-      //TODO: rework to make sure that the user's id is also stored in async storage and populate it to redux slice
-      if (loggedIn === 'true' && userId) {
-        router.replace('/(tabs)');
-        dispatch(setUserId(userId))
-      } else {
-        router.replace('/login');
-      }
-    };
-
-    if (loaded) {
-      SplashScreen.hideAsync();
-      checkLoginStatus();
-    }
-  }, [loaded, router]);
 
   if (!loaded) {
     return null;
@@ -48,14 +24,38 @@ export default function RootLayout() {
 
   return (
     <Provider store={store}>
-      {/* Wrap the ThemeProvider and Stack with the Redux Provider */}
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="login" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-      </ThemeProvider>
+      <RootContent colorScheme={"light"} />
     </Provider>
+  );
+}
+
+function RootContent({ colorScheme }: { colorScheme: 'light' | 'dark' }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      const loggedIn = await AsyncStorage.getItem('isLoggedIn');
+      const userId = await AsyncStorage.getItem('userId');
+
+      if (loggedIn === 'true' && userId) {
+        router.replace('/(tabs)');
+        store.dispatch({ type: 'user/setUserId', payload: userId });
+      } else {
+        router.replace('/login');
+      }
+    };
+
+    SplashScreen.hideAsync();
+    checkLoginStatus();
+  }, [router]);
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </ThemeProvider>
   );
 }
