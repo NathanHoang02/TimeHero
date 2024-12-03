84623bf991c21e8478e6249f2cbea5d07b1f6d8d
4rcher0116 <100230824+4rcher0116@users.noreply.github.com>
2024-12-02 19:53:07 -0600

    Async fetching of userId

--- TimeHero/app/(tabs)/index.tsx
+++ TimeHero/app/(tabs)/index.tsx
@@ -9,16 +9,27 @@ import { useSelector } from 'react-redux';
 import { fetchUserInfo } from '@/store/userSlice';
 import { RootState, useAppDispatch } from '@/store/store';
 import { ID_MAP } from '@/constants/ID_MAP';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function HomeScreen() {
   const dispatch = useAppDispatch();
   const $userId = useSelector((state: RootState) => state.user.userInfo?.id);
 
-
   useEffect(() => {
-    if ($userId)dispatch(fetchUserInfo($userId))
-  }, [])
+    const getUserIdFromStorage = async () => {
+      try {
+        const storedUserId = await AsyncStorage.getItem('userId');
+        if (storedUserId) {
+          // If a userId is found, dispatch the fetchUserInfo action
+          dispatch(fetchUserInfo(storedUserId));
+        }
+      } catch (error) {
+        console.error('Failed to load userId from AsyncStorage:', error);
+      }
+    };
 
+    getUserIdFromStorage();
+  }, [dispatch]);
 
   return (
     <ParallaxScrollView
