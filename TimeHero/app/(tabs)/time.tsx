14870c629c93a8ebb57dec9670d9b9d3a2ae0c87
Justin Nguyen <Jnguy129@gmail.com>
2024-12-02 19:24:09 -0600

    Connecting time page to backend

--- TimeHero/app/(tabs)/time.tsx
+++ TimeHero/app/(tabs)/time.tsx
@@ -5,30 +5,40 @@ import HandleRedeemButton from '@/components/handleRedeemButton';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 import { RootState, useAppDispatch } from '@/store/store';
-import { fetchUserInfo } from '@/store/userSlice';
+import { fetchEarnedTime, fetchUserInfo, updateEarnedTime } from '@/store/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useStoreRootState } from 'expo-router/build/global-state/router-store';
 
 export default function TimeScreen() {
-  const formatTime = (timeInSeconds: number) => {
-    const hours = Math.floor(timeInSeconds / 3600);
-    const minutes = Math.floor((timeInSeconds % 3600) / 60);
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
-  };
+  // const formatTime = (timeInSeconds: number) => {
+  //   const hours = Math.floor(timeInSeconds / 3600);
+  //   const minutes = Math.floor((timeInSeconds % 3600) / 60);
+  //   return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+  // };
 
   const dispatch = useAppDispatch();
 
-  // Retrieve UserID from Redux
-  const userId = useSelector((state: RootState) => state.user.userId);
-
-  // Get accumulateTime from userinfo
-  const $userAccumulatedTime = useSelector((state: RootState) => state.user.userInfo?.accumulatedTime) ?? null;
+  const $userId = useSelector((state: RootState) => state.user.userId);
+  const $earnedTime = useSelector((state: RootState) => state.user.userInfo?.accumulatedTime) ?? 0;
 
   useEffect(() => {
-    if (userId) {
-        dispatch(fetchUserInfo(userId));
+    if ($userId) {
+        dispatch(fetchUserInfo($userId));
     }
-  }, [dispatch, userId]);
+  }, [dispatch, $userId]);
+
+  const handleTimeUpdate = async (newTime: number) => {
+    if ($userId){
+      await dispatch(updateEarnedTime({userId: $userId, newTime}));
+      dispatch(fetchEarnedTime($userId));
+    }
+  };
+
+  const formatTime = (timeInSeconds: number) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    return `${String(hours).padStart(2, '0')}hr :${String(minutes).padStart(2, '0')}mins`;
+  };
 
   return (
     <ParallaxScrollView
@@ -38,11 +48,11 @@ export default function TimeScreen() {
       <Text style={styles.title}>Time Earned</Text>
 
       <View style={styles.timerBackground}>
-        <Text style={styles.timerText}>{formatTime($userAccumulatedTime ?? 0)}</Text>
+        <Text style={styles.timerText}>{formatTime($earnedTime ?? 0)}</Text>
       </View>
 
       <View style={styles.buttonContainer}>
-        <HandleRedeemButton totalTime={$userAccumulatedTime ?? 0}/>
+        <HandleRedeemButton totalTime={$earnedTime} onTimeUpdate={handleTimeUpdate}/>
       </View>
     </ParallaxScrollView>
   );
